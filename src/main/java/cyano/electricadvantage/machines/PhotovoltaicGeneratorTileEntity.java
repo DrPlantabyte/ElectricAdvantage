9ac6c805764033adc0d07484e4e7459de23f518e
CCHall <explosivegnome@yahoo.com>
2015-08-08 17:57:08 -0400

    finished implementation of solar panel

--- src/main/java/cyano/electricadvantage/machines/PhotovoltaicGeneratorTileEntity.java
+++ src/main/java/cyano/electricadvantage/machines/PhotovoltaicGeneratorTileEntity.java
@@ -3,14 +3,18 @@ package cyano.electricadvantage.machines;
 import cyano.electricadvantage.init.Power;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 
 public class PhotovoltaicGeneratorTileEntity extends ElectricGeneratorTileEntity {
-	private final float PI = (float)Math.PI;
-	private final float SUM_OF_SINE_TICKS = 24000 / PI;
-	public final float SOLAR_PER_DAY = 25000; // half the energy of a piece of coal per day
-	public final float MAX_SOLAR_OUTPUT = SOLAR_PER_DAY / SUM_OF_SINE_TICKS;
+
+	private static final float PI = (float)Math.PI;
+	private static final float SUM_OF_SINE_TICKS = 24000 / PI / 8;
+	public static final float SOLAR_PER_DAY = 25000; // half the energy of a piece of coal per day
+	public static final float MAX_SOLAR_OUTPUT = PI * SOLAR_PER_DAY / SUM_OF_SINE_TICKS;
+	public static final float BLOCKLIGHT_FACTOR = 0.1f; // light from blocks less effective than sunlight
 	private float powerOutput = 0;
 	
 	public PhotovoltaicGeneratorTileEntity() {
@@ -20,6 +24,11 @@ public class PhotovoltaicGeneratorTileEntity extends ElectricGeneratorTileEntity
 	@Override
 	public float getPowerOutput() {
 		return powerOutput;
+	}
+	
+	@Override
+	public int getComparatorOutput(){
+		return (int)(15 * powerOutput);
 	}
 
 	@Override
@@ -37,31 +46,47 @@ public class PhotovoltaicGeneratorTileEntity extends ElectricGeneratorTileEntity
 		// do nothing
 	}
 
-	final int[] da = new int[1];
+	final int[] dataArray = new int[1];
 	@Override
 	public int[] getDataFieldArray() {
-		return da;
+		return dataArray;
 	}
 
 	@Override
 	public void onDataFieldUpdate() {
-		powerOutput = Float.intBitsToFloat(da[0]);
+		powerOutput = Float.intBitsToFloat(dataArray[0]);
 	}
 
 	@Override
 	public void prepareDataFieldsForSync() {
-		da[0] = Float.floatToIntBits(powerOutput);
+		dataArray[0] = Float.floatToIntBits(powerOutput);
 	}
 
 	@Override
 	public void tickUpdate(boolean isServer) {
-		if(isServer){
-			World world = getWorld();
-			float sinTime = MathHelper.sin((float)world.getWorldTime() * 2.61799E-4f); // (worldTime * pi / 12000)
-			float power = Math.max(0,MAX_SOLAR_OUTPUT * sinTime);
-			this.addEnergy(power, Power.ELECTRIC_POWER);
-			powerOutput = power / MAX_SOLAR_OUTPUT;
+		// do nothing
+	}
+	
+	@Override
+	public void powerUpdate(){
+		float solar = getLightIntensityAt(getPos(),getWorld());
+		float power = Math.max(0,MAX_SOLAR_OUTPUT * solar);
+		this.addEnergy(power, Power.ELECTRIC_POWER);
+		powerOutput = power / MAX_SOLAR_OUTPUT;
+		
+		super.powerUpdate();
+	}
+	
+	public static float getLightIntensityAt(BlockPos pos, World w){
+		float light;
+		if(w.canSeeSky(pos) && (w.provider.getHasNoSky() == false)){
+			// outdoor light imposes no penalty
+			light = MathHelper.sin((float)w.getWorldTime() * 2.61799E-4f); // (worldTime * pi / 12000);
+		} else {
+			float blockLight = w.getLightFromNeighbors(pos) * 6.66666E-2f; // block light / 15
+			light = BLOCKLIGHT_FACTOR * blockLight;
 		}
+		return light;
 	}
 
 }

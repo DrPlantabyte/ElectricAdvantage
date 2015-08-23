5c38ee3a7a31973954260640413aff605a2bf64f
Cyanobacterium <explosivegnome@yahoo.com>
2015-08-22 22:19:51 -0400

    incremented version number

--- src/main/java/cyano/electricadvantage/ElectricAdvantage.java
+++ src/main/java/cyano/electricadvantage/ElectricAdvantage.java
@@ -35,7 +35,7 @@ public class ElectricAdvantage
 	/** The display name for this mod */
 	public static final String NAME = "Electric Advantage";
 	/** The version of this mod, in the format major.minor.update */
-	public static final String VERSION = "0.0.4";
+	public static final String VERSION = "0.0.5";
 
 	public static ElectricAdvantage INSTANCE = null;
 

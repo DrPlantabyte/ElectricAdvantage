e546e2ac6c745f5b36432dfd215d6fef6fde1ff5
Cyanobacterium <explosivegnome@yahoo.com>
2015-09-18 14:49:37 -0400

    fixed block bounds bug

--- src/main/java/cyano/electricadvantage/machines/LEDBlock.java
+++ src/main/java/cyano/electricadvantage/machines/LEDBlock.java
@@ -1,19 +1,24 @@
 package cyano.electricadvantage.machines;
 
+import java.util.List;
+
 import cyano.electricadvantage.blocks.ElectricConduitBlock;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class LEDBlock extends ElectricConduitBlock implements ITileEntityProvider{
 
-
+	private final float pipeRadius = 2f/16f;
 	public static final PropertyBool LIT = PropertyBool.create("lit");
 	
 	public LEDBlock(){
@@ -59,4 +64,104 @@ public class LEDBlock extends ElectricConduitBlock implements ITileEntityProvide
 	public boolean isPowerSink(){
 		return true;
 	}
+	
+
+    /**
+     * Calculates the collision boxes for this block.
+     */
+	@Override
+    public void setBlockBoundsBasedOnState(final IBlockAccess world, final BlockPos coord) {
+        final boolean connectNorth = this.canConnectTo(world,coord,EnumFacing.NORTH, coord.north());
+        final boolean connectSouth = this.canConnectTo(world,coord,EnumFacing.SOUTH, coord.south());
+        final boolean connectWest = this.canConnectTo(world,coord,EnumFacing.WEST, coord.west());
+        final boolean connectEast = this.canConnectTo(world,coord,EnumFacing.EAST, coord.east());
+        final boolean connectUp = this.canConnectTo(world,coord,EnumFacing.UP, coord.up());
+        boolean connectDown = this.canConnectTo(world,coord,EnumFacing.DOWN, coord.down());
+        
+        if(!(connectNorth || connectSouth || connectWest || connectEast || connectUp || connectDown)){
+        	connectDown = true;
+        }
+        
+        float radius = pipeRadius;
+        float rminus = 0.5f - radius;
+        float rplus = 0.5f + radius;
+        
+        float x1 = rminus;
+        float x2 = rplus;
+        float y1 = rminus;
+        float y2 = rplus;
+        float z1 = rminus;
+        float z2 = rplus;
+        if (connectNorth) {
+            z1 = 0.0f;
+        }
+        if (connectSouth) {
+            z2 = 1.0f;
+        }
+        if (connectWest) {
+            x1 = 0.0f;
+        }
+        if (connectEast) {
+            x2 = 1.0f;
+        }
+        if(connectDown){
+        	y1 = 0.0f;
+        }
+        if(connectUp){
+        	y2 = 1.0f;
+        }
+        this.setBlockBounds(x1, y1, z1, x2, y2, z2);
+    }
+
+    /**
+     * Calculates the collision boxes for this block.
+     */
+	@Override
+    public void addCollisionBoxesToList(final World world, final BlockPos coord, 
+    		final IBlockState bs, final AxisAlignedBB box, final List collisionBoxList, 
+    		final Entity entity) {
+        final boolean connectNorth = this.canConnectTo(world,coord,EnumFacing.NORTH, coord.north());
+        final boolean connectSouth = this.canConnectTo(world,coord,EnumFacing.SOUTH, coord.south());
+        final boolean connectWest = this.canConnectTo(world,coord,EnumFacing.WEST, coord.west());
+        final boolean connectEast = this.canConnectTo(world,coord,EnumFacing.EAST, coord.east());
+        final boolean connectUp = this.canConnectTo(world,coord,EnumFacing.UP, coord.up());
+        boolean connectDown = this.canConnectTo(world,coord,EnumFacing.DOWN, coord.down());
+        
+        if(!(connectNorth || connectSouth || connectWest || connectEast || connectUp || connectDown)){
+        	connectDown = true;
+        }
+        
+        float radius = pipeRadius;
+        float rminus = 0.5f - radius;
+        float rplus = 0.5f + radius;
+        
+        this.setBlockBounds(rminus, rminus, rminus, rplus, rplus, rplus);
+        super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+
+        if(connectUp){
+            this.setBlockBounds(rminus, rminus, rminus, rplus, 1f, rplus);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+        if(connectDown){
+            this.setBlockBounds(rminus, 0f, rminus, rplus, rplus, rplus);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+        if(connectEast){
+            this.setBlockBounds(rminus, rminus, rminus, 1f, rplus, rplus);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+        if(connectWest){
+            this.setBlockBounds(0f, rminus, rminus, rplus, rplus, rplus);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+        if(connectSouth){
+            this.setBlockBounds(rminus, rminus, rminus, rplus, rplus, 1f);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+        if(connectNorth){
+            this.setBlockBounds(rminus, rminus, 0f, rplus, rplus, rplus);
+            super.addCollisionBoxesToList(world, coord, bs, box, collisionBoxList, entity);
+        }
+    }
+	
 }

ce64a4b455bac44986e6a392f7dfc32da70cef32
Cyanobacterium <explosivegnome@yahoo.com>
2015-09-20 09:00:34 -0400

    balance tweak

--- src/main/java/cyano/electricadvantage/init/TreasureChests.java
+++ src/main/java/cyano/electricadvantage/init/TreasureChests.java
@@ -4,6 +4,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.WeightedRandomChestContent;
 import net.minecraftforge.common.ChestGenHooks;
 import cyano.poweradvantage.PowerAdvantage;
+import cyano.poweradvantage.RecipeMode;
 
 public abstract class TreasureChests {
 
@@ -20,6 +21,13 @@ public abstract class TreasureChests {
 		addChestLoot(new ItemStack(Items.alkaline_battery,1),2f,1,1);
 		addChestLoot(new ItemStack(Items.lithium_battery,1),1f,1,1);
 		
+		if(PowerAdvantage.recipeMode == RecipeMode.APOCALYPTIC){
+			addChestLoot(new ItemStack(Items.integrated_circuit,1),2.5f,1,5);
+			addChestLoot(new ItemStack(Items.control_circuit,1),1.25f,1,1);
+			addChestLoot(new ItemStack(Blocks.photovoltaic_generator,1),0.5f,1,1);
+			addChestLoot(new ItemStack(Items.solder,3),2f,1,1);
+		}
+		
 		initDone = true;
 	}
 	
@@ -32,7 +40,7 @@ public abstract class TreasureChests {
 	}
 	
 	private static void addChestLoot(ItemStack item, float weight, int number, int range){
-		WeightedRandomChestContent loot = makeChestLootEntry(item,Math.max(1, (int)(weight*PowerAdvantage.chestLootFactor)),number,number+range);
+		WeightedRandomChestContent loot = makeChestLootEntry(item,Math.max(1, Math.max(1, (int)(weight*PowerAdvantage.chestLootFactor))),number,number+range);
 		if(loot != null){
 			ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH).addItem(loot);
 			ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST).addItem(loot);
